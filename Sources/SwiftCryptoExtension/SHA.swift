359d6928daae640e978756f42015700883ce5d00
Ji-Hwan KIM <nodealdev@gmail.com>
2023-10-15 17:53:36 +0900

    [Script push]

--- Sources/SwiftCryptoExtension/SHA.swift
+++ Sources/SwiftCryptoExtension/SHA.swift
@@ -11,17 +11,17 @@ import CryptoKit
 public final class SHA {
     private init() {}
     
-    public static func hash(plain text: String) throws -> String {
+    public static func hash512(plain text: String) throws -> String {
         guard let data = text.data(using: .utf8) else {
             throw SwiftCryptoExtensionLocalError.SHA.PlainTextError
         }
         let hash = CryptoKit.SHA512.hash(data: data)
         let hashData = Data(hash)
-        if let s = String(data: hashData, encoding: .utf8) {
-            return s
-        } else {
-            throw SwiftCryptoExtensionLocalError.SHA.DigestDataError
+        var hex: String = ""
+        for byte in hashData {
+            hex += .init(format: "%02x", byte)
         }
         
+        return hex
     }
 }
